033393c578d49f46a3ec87170e1ed97d1995393b
MrVants <andrevants@gmail.com>
2019-05-02 15:18:00 -0300

    Adding products stub data for testing

---
+++ FashionShowcaseTests/ProductsSpec.swift
@@ -0,0 +1,30 @@
+//
+//  ProductsSpec.swift
+//  FashionShowcaseTests
+//
+//  Created by André Vants Soares de Almeida on 02/05/19.
+//  Copyright © 2019 Tinkerskull. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+@testable import FashionShowcase
+
+class ProductsSpec: QuickSpec {
+
+    override func spec() {
+        
+        var products: [Product] = []
+        
+        beforeSuite {
+            products = ProductsAPIStub().mockProducts() ?? []
+        }
+        
+        describe("Products") {
+            it("is correctly decoded from raw data") {
+                expect(products).toNot(beEmpty())
+            }
+        }
+    }
+}
---
+++ FashionShowcaseTests/Stubs/ProductsAPIStub.swift
@@ -0,0 +1,34 @@
+//
+//  File.swift
+//  FashionShowcaseTests
+//
+//  Created by André Vants Soares de Almeida on 02/05/19.
+//  Copyright © 2019 Tinkerskull. All rights reserved.
+//
+
+import Foundation
+
+@testable import FashionShowcase
+
+class ProductsAPIStub: ProductsAPI {
+    
+    func fetchProducts(completion: @escaping (Result<[Product], Error>) -> Void) {
+        if let products = mockProducts() {
+            completion(.success(products))
+        }
+    }
+    
+    func mockProducts() -> [Product]? {
+        if let path = Bundle(for: ProductsAPIStub.self).path(forResource: "ProductsDtoStub", ofType: "json") {
+            do {
+                let fileUrl = URL(fileURLWithPath: path)
+                let fileData = try Data(contentsOf: fileUrl, options: .uncached)
+                let decodedData = try JSONDecoder().decode(ProductsDto.self, from: fileData)
+                return decodedData.products
+            } catch {
+                print("[ProductsAPIStub] Failed to read local test data: \(error.localizedDescription)")
+            }
+        }
+        return nil
+    }
+}
