8d94f3acc755a4391dd5a080f34112824e7e18ec
MrVants <andrevants@gmail.com>
2019-05-02 12:59:12 -0300

    Basic project structure

---
+++ FashionShowcase/Presentation/ShoppingCartPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  ShoppingCartPresenter.swift
+//  FashionShowcase
+//
+//  Created by André Vants Soares de Almeida on 02/05/19.
+//  Copyright © 2019 Tinkerskull. All rights reserved.
+//
+
+import Foundation
