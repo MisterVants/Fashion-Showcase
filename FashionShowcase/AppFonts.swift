cf8fb854e8156df9cb08d1289ba3aa1282171b3d
MrVants <andrevants@gmail.com>
2019-05-05 11:41:49 -0300

    Added custom font files

--- FashionShowcase/AppFonts.swift
+++ FashionShowcase/AppFonts.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2019 Tinkerskull. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIFont {
+    
+    static func gothamBlack(_ fontSize: CGFloat) -> UIFont {
+        let fontName = "Gotham-Black"
+        guard let font = UIFont(name: fontName, size: fontSize) else { fatalError("Font file not found in bundle for font \(fontName)") }
+        return font
+    }
+    
+    static func gothamBold(_ fontSize: CGFloat) -> UIFont {
+        let fontName = "Gotham-Bold"
+        guard let font = UIFont(name: fontName, size: fontSize) else { fatalError("Font file not found in bundle for font \(fontName)") }
+        return font
+    }
+    
+    static func gothamMedium(_ fontSize: CGFloat) -> UIFont {
+        let fontName = "Gotham-Medium"
+        guard let font = UIFont(name: fontName, size: fontSize) else { fatalError("Font file not found in bundle for font \(fontName)") }
+        return font
+    }
+    
+    static func gothamLight(_ fontSize: CGFloat) -> UIFont {
+        let fontName = "Gotham-Light"
+        guard let font = UIFont(name: fontName, size: fontSize) else { fatalError("Font file not found in bundle for font \(fontName)") }
+        return font
+    }
+    
+    static func gothamThin(_ fontSize: CGFloat) -> UIFont {
+        let fontName = "Gotham-Thin"
+        guard let font = UIFont(name: fontName, size: fontSize) else { fatalError("Font file not found in bundle for font \(fontName)") }
+        return font
+    }
+}
